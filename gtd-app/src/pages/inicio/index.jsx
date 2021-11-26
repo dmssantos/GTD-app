bc43046e58b3abbcdd6ed81e483e742b7a9c3d6f
dmssantos <dms.santos_@hotmail.com>
2021-11-26 11:45:15 -0300

    destest

--- gtd-app/src/pages/inicio/index.jsx
+++ gtd-app/src/pages/inicio/index.jsx
@@ -8,13 +8,13 @@ import Card from '../../components/card/index';
 //Function
 export default function Inicio() {
   return (
-    <test>
+    <>
       <Header />
       <Card
         title="Caixa de Entrada"
         tableTitle={['Tarefa', 'Prazo?', 'Prioritária?', 'Projeto', 'Contexto']}
         tableContent={['Tarefa-1', '22/12/22', true, 'Projeto-1', 'Rapida']}
       />
-    </test>
+    </>
   )
 };
